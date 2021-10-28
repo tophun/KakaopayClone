24b58898965c9d51f1343d3f13ae800f0883b407
Sanghoon Jung <imtophun@gmail.com>
2021-10-28 10:06:53 +0900

    feat: CustomTabbar Initialize & setup 추가

--- KakaopayClone/Views/CustomTabbar.swift
+++ KakaopayClone/Views/CustomTabbar.swift
@@ -6,3 +6,39 @@
 //
 
 import UIKit
+import SnapKit
+import Then
+
+class CustomTabbar: UIView {
+  
+  private lazy var hStack = UIStackView().then {
+    $0.axis = .horizontal
+    $0.distribution = .fillEqually
+  }
+  
+  // MARK: Initialize
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    setup()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: Setup
+  private func setup() {
+    setupView()
+    setupLayout()
+  }
+  
+  private func setupView() {
+    addSubview(hStack)
+  }
+  
+  private func setupLayout() {
+    hStack.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+    }
+  }
+}
